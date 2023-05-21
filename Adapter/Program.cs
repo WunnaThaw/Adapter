78a8ede6f20881609398bcb6f873628c00531953
WunnaThaw <wunna.pe@gmail.com>
2023-05-21 21:41:35 +0630

    Adapter Design Pattern

---
+++ Adapter/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Adapter
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ITarget Itarget = new EmployeeAdapter();
+            ThirdPartyBillingSystem client = new ThirdPartyBillingSystem(Itarget);
+            client.ShowEmployeeList();
+
+            Console.ReadKey();
+        }
+    }
+}
